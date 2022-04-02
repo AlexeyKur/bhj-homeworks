8a6f81c7c31977f015c751fae8cbcc7f8e8ffe58
Alexey Kurenkov <kurenkov.alexey89@yandex.ru>
2022-04-03 00:41:39 +0500

    homework 1.2 fixed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,8 +1,8 @@
 const sliderItems = Array.from(document.querySelectorAll('.slider__item'));
 const sliderDots = Array.from(document.querySelectorAll('.slider__dot'));
 const sliderDotsButton = document.querySelector('.slider__dots');
-const ArrowPrev = document.querySelector('.slider__arrow_prev');
-const ArrowNext = document.querySelector('.slider__arrow_next');
+const arrowPrev = document.querySelector('.slider__arrow_prev');
+const arrowNext = document.querySelector('.slider__arrow_next');
 
 let slideIndex = 0;
 
@@ -11,33 +11,30 @@ showSliders(slideIndex);
 function showSliders (n) {
   if(n >= sliderItems.length) {
     slideIndex = 0;
+  } else if(n < 0) {
+    slideIndex = sliderItems.length - 1;
   }
-
-  for(let i = 0; i < sliderItems.length; i++) {
-    sliderItems[i].classList.remove('slider__item_active');
-  }
-
-  for(let i = 0; i < sliderDots.length; i++) {
-    sliderDots[i].classList.remove('slider__dot_active');
-  }
+  let i = sliderItems.findIndex(item => item.classList.contains('slider__item_active'));
+  sliderItems[i].classList.remove('slider__item_active');
+  sliderDots[i].classList.remove('slider__dot_active');
   
   sliderItems[slideIndex].classList.add('slider__item_active');
   sliderDots[slideIndex].classList.add('slider__dot_active');
 }
 
 function plusSlides (n) {
-  showSliders(slideIndex += 1);
+  showSliders(slideIndex += n);
 }
 
 function itemDot (n) {
   showSliders(slideIndex = n);
 }
 
-ArrowNext.onclick = () => {
+arrowNext.onclick = () => {
   plusSlides(1);
 }
 
-ArrowPrev.onclick = () => {
+arrowPrev.onclick = () => {
   plusSlides(-1);
 }
 
